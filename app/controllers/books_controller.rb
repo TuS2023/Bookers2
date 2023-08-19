bcada5e6c930050f057f82f0e259272102b7a026
TuS2023 <138543864+TuS2023@users.noreply.github.com>
2023-08-19 08:07:04 +0000

    [update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,47 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
   def index
+    @book = Book.new
+    @user = current_user
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+    @books = Book.new
+    @user = @book.user
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get 'homes/about', to: 'homes#about', as: 'about'
-  resources :users, only: [:edit, :show]
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:edit, :show, :index, :update]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
